4743751667167686d42252f71490a9c1d6e96bd3
XannyB <xannyb@gmail.com>
2014-01-30 14:19:37 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 //JS will go here.
 //TODO create class
+function cardDamageValue(strength,speed,weight,type){
+     var obj = {};
+ 
+       obj.strength=strength;
+       obj.speed=speed;
+       obj.weight=weight;
+       obj.type=type;
+ 
+      return obj;
+}
+ 
+var cardDamageValue1 = cardDamageValue(10, 5, 4, ‘Lizard’);
+var cardDamageValue2 = cardDamageValue(5, 4, 10, ‘Whale’);
+ 
+//You could then say... let's play!
+ 
+If (cardDamageValue1.speed > cardDamageValue2.speed) {
+          console.log(‘Player 1 wins');
+}
