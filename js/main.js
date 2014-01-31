e92b1cb2b11ffa47d878d42334247977e9e2c349
XannyB <xannyb@gmail.com>
2014-01-31 13:54:53 +0000

    Made it work

--- js/main.js
+++ js/main.js
@@ -11,11 +11,14 @@ function cardDamageValue(strength,speed,weight,type){
       return obj;
 }
  
-var cardDamageValue1 = cardDamageValue(10, 5, 4, ‘Lizard’);
-var cardDamageValue2 = cardDamageValue(5, 4, 10, ‘Whale’);
- 
 //You could then say... let's play!
  
-If (cardDamageValue1.speed > cardDamageValue2.speed) {
-          console.log(‘Player 1 wins');
+function showWinner (){
+
+var cardDamageValue1 = cardDamageValue(10, 5, 4, 'Lizard');
+var cardDamageValue2 = cardDamageValue(5, 4, 10, 'Whale');
+
+if (cardDamageValue1.speed > cardDamageValue2.speed) {
+          console.log('Player 1 wins');
+}
 }
